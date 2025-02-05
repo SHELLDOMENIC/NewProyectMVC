807d87186a23b0314bec783716dbc3595ad97cf8
OswaldoBifarini <jgbifarini@gmail.com>
2025-02-05 14:13:46 -0500

    Controlador

--- src/main/java/espoch/edu/ec/newproyectmvcdom/Controlador/Controlador.java
+++ src/main/java/espoch/edu/ec/newproyectmvcdom/Controlador/Controlador.java
@@ -1,10 +1,21 @@
 
 package espoch.edu.ec.newproyectmvcdom.Controlador;
 
-/**
- *
- * @author and_j
- */
-public class Controlador {
+import espoch.edu.ec.newproyectmvcdom.Vista.Vista;
+
+    
+    public class Controlador {
+    //lineas de coneccion con vista 
+  private Vista vista; 
+  private GestorTarea modelo;
+
+    public Controlador(Vista vista, GestorTarea modelo) {
+        this.vista = vista;
+        //lineas de conexion
+        this.modelo = new GestorTarea();
+    }
+    public void procesoGestorTarea (){
+    //si los datos son corectos pasa el modelo, caso contrario los devuelve a la vista
+    }
     
 }
